2a0528429911072471d7e49e786ddb0cc650db72
Matias Pettersen <matias.pettersen@statnett.no>
2018-12-17 18:51:49 +0100

    update port

--- serve.py
+++ serve.py
@@ -5,7 +5,7 @@ from app import app
 
 from app.app import doStuff
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     thread = Thread(target=doStuff, args=(10,))
     thread.start()
-    waitress.serve(app)
+    waitress.serve(app, port=8041)
